7cb07460993c7cfdf0837b8381bb0ebcdaea2832
James Nguyen <jnguyen@jnguyens-MacBook-Pro.local>
2019-09-05 10:29:04 -0700

    Add functionalities to list users, show/display policies, enable/disable audit, calculating hmac-sha256 for auditing

--- post_admin_account.sh
+++ post_admin_account.sh
@@ -10,13 +10,12 @@ DEFAULT_ADMIN_USERNAME="admin"
 _HTTP_RET_CODE_LABEL="HTTP return code:"
 
 # arguments
-VAULT_TOKEN=$1
-VAULT_URL=$2
-ADMIN_USERNAME=$3
+VAULT_URL=$1
+ADMIN_USERNAME=$2
 
 # Usage
-if [[ "$1" == "h" || "$1" == "help" || "$1" == "--help" || -z "$VAULT_TOKEN" ]]; then
-	echo "Usage: $0 <Vault Token> <Vault URL> <admin username>"
+if [[ "$1" == "h" || "$1" == "help" || "$1" == "--help" ]]; then
+	echo "Usage: $0 <Vault URL> <admin username>"
 	echo "       Use . for the default value for any parameter but the 1st argument <Vault Token> must be specified"
 	exit 1
 fi
@@ -73,11 +72,10 @@ change_password() {
 
 login() {
 	echo "Login"
-	echo "Enter the password for $ADMIN_USER to login:"
+	echo "Enter the password for \"$ADMIN_USERNAME\" to login:"
 	read -s LOGINPASSWD
 
 	ret=$(curl -SsL \
-		-H "X-Vault-Token: $VAULT_TOKEN" \
 		-X POST \
 		-d "{ \"password\" : \"$LOGINPASSWD\" }" \
 		"$VAULT_URL/v1/auth/userpass/login/$ADMIN_USERNAME" \
@@ -88,23 +86,139 @@ login() {
 	else
 		client_token=$(echo $ret | jq -r '.auth.client_token')
 		echo "Login successfully, client Token: $client_token"
+		echo "Using the login token now..."
+		VAULT_TOKEN=$client_token
 	fi
+}
+
+show_policies() {
+	echo "Show policy"
+
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		-X GET \
+		"$VAULT_URL/v1/auth/userpass/users/$ADMIN_USERNAME" \
+	)
+
+	#echo $ret
+
+	policies=$(echo $ret | cut -d "[" -f2 | cut -d "]" -f1)
+
+	echo $policies
+}
+
+display_a_policy() {
+	echo "Display a policy"
+	read -p "Enter the policy name: " polname
+
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		-X GET \
+		"$VAULT_URL/v1/sys/policy/$polname" \
+	)
+
+	rules=$(echo $ret | jq -r '.rules')
+	printf "%s\n" $rules
+}
+
+hmac_sha256() {
+	echo "calculate hmac-sha256"
+	read -p "Enter the audit path: " auditpath
+	read -p "Enter the string to calculate hmac-sha256: " inputstring
+
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		-X POST \
+		-d "{ \"input\" : \"$inputstring\" }" \
+		"$VAULT_URL/v1/sys/audit-hash/$auditpath" \
+	)
+
+	hmac_sha256_val=$(echo $ret | jq -r '.hash')
+
+	echo $hmac_sha256_val
+}
+
+enable_audit_file() {
+	echo "Enable audit file"
+	read -p "Enter the audit path (name of audit): " auditpath
+	read -p "Enter the audit file path: " auditfilepath
+
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		-X PUT \
+		-d "{\"type\":\"file\",\"options\":{\"file_path\":\"$auditfilepath\"}}" \
+		"$VAULT_URL/v1/sys/audit/$auditpath" \
+	)
+
+	if [[ $ret != "" ]]; then
+		echo "Error in enabling audit -file"
+		echo $ret
+	fi
+}
+
+
+disable_audit_file() {
+	echo "Disable audit file"
+	read -p "Enter the audit path (name of audit): " auditpath
+
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		-X DELETE \
+		"$VAULT_URL/v1/sys/audit/$auditpath" \
+	)
+
+	if [[ $ret != "" ]]; then
+		echo "Error in enabling audit -file"
+		echo $ret
+	fi
+}
+
+list_file_audit()
+{
+	echo "List file audits"
+	
+	ret=$(curl -SsL \
+		-H "X-Vault-Token: $VAULT_TOKEN" \
+		"$VAULT_URL/v1/sys/audit" \
+	)
+
+	echo $ret | jq
 }
 
 # inputs
 PS3='Please enter your choice: '
-options=("List Users" "Change User's Password" "Login" "Quit")
+options=("Login" "Change User's Password" "List Users" "Show policies" \
+	 "Display a policy" "List file audits" "Enable audit file" \
+	 "Disable audit file" "Calculate hmac-sha256" "Quit")
 select opt in "${options[@]}"
 do
 	case $opt in
-		"List Users")
-			list_users
+		"Login")
+			login
 			;;
 		"Change User's Password")
 			change_password
 			;;
-		"Login")
-			login
+		"List Users")
+			list_users
+			;;
+		"Show policies")
+			show_policies
+			;;
+		"Display a policy")
+			display_a_policy
+			;;
+		"List file audits")
+			list_file_audit
+			;;
+		"Enable audit file")
+			enable_audit_file
+			;;
+		"Disable audit file")
+			disable_audit_file
+			;;
+		"Calculate hmac-sha256")
+			hmac_sha256
 			;;
 		"Quit")
 			break
